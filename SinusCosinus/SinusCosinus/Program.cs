86a7c963601325152b3047af53d568ebfb824044
Belov-AI <Belov.Alexander@urfu.ru>
2024-09-30 12:53:03 +0500

    Синус и косинус - версия 1

---
+++ SinusCosinus/SinusCosinus/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SinusCosinus
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double angleInDegees = 15;
+            double angleInRadians = angleInDegees * Math.PI / 180;
+            double sin = Math.Sin(angleInRadians);
+            double cos = Math.Cos(angleInRadians);
+
+            Console.WriteLine("sin(15°) = " + Math.Round(sin, 3));
+            Console.WriteLine("cos(15°) = " + Math.Round(cos, 3));
+
+            angleInDegees = 37;
+            angleInRadians = angleInDegees * Math.PI / 180;
+            sin = Math.Sin(angleInRadians);
+            cos = Math.Cos(angleInRadians);
+
+            Console.WriteLine("sin(37°) = " + Math.Round(sin, 3));
+            Console.WriteLine("cos(37°) = " + Math.Round(cos, 3));
+
+            angleInDegees = 113;
+            angleInRadians = angleInDegees * Math.PI / 180;
+            sin = Math.Sin(angleInRadians);
+            cos = Math.Cos(angleInRadians);
+
+            Console.WriteLine("sin(113°) = " + Math.Round(sin, 3));
+            Console.WriteLine("cos(113°) = " + Math.Round(cos, 3));
+
+            Console.ReadKey();
+        }
+    }
+}
