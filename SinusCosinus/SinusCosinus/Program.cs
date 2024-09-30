2e15d9f11b448d400849c64d6aef088803a143d7
Belov-AI <Belov.Alexander@urfu.ru>
2024-09-30 13:02:12 +0500

    Синус и косинус - версия 2

--- SinusCosinus/SinusCosinus/Program.cs
+++ SinusCosinus/SinusCosinus/Program.cs
@@ -34,6 +34,17 @@ namespace SinusCosinus
             Console.WriteLine("sin(113°) = " + Math.Round(sin, 3));
             Console.WriteLine("cos(113°) = " + Math.Round(cos, 3));
 
+            Console.WriteLine();
+            Console.WriteLine("Введите значение угла в градусах");
+            angleInDegees = double.Parse(Console.ReadLine());
+
+            angleInRadians = angleInDegees * Math.PI / 180;
+            sin = Math.Sin(angleInRadians);
+            cos = Math.Cos(angleInRadians);
+
+            Console.WriteLine("sin(" + angleInDegees + "°) = " + Math.Round(sin, 3));
+            Console.WriteLine("cos(" + angleInDegees + "°)) = " + Math.Round(cos, 3));
+
             Console.ReadKey();
         }
     }
