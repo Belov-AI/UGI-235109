2ce38990c030e2c0cdd5009089283d0e1a643155
Belov-AI <Belov.Alexander@urfu.ru>
2024-10-07 13:49:03 +0500

    Задание 5 выполнено

---
+++ Task05/Task05/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double y = F(3, 2) + F(7, 5) + F(2, 7) + F(5, 11);
+            Console.WriteLine("x = " + Math.Round(y,3));
+
+            Console.ReadKey();
+        }
+
+        static double F(double a, double b)
+        {
+            return Math.Sqrt((a+b+Math.Sqrt(a))/b);
+        }
+    }
+}
