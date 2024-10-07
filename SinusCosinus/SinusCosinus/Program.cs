e0f1f59da24bbe3dd113c6aa068dba95e01a7769
Belov-AI <Belov.Alexander@urfu.ru>
2024-10-07 13:37:07 +0500

    Версия 3. Извдечение метода

--- SinusCosinus/SinusCosinus/Program.cs
+++ SinusCosinus/SinusCosinus/Program.cs
@@ -10,42 +10,28 @@ namespace SinusCosinus
     {
         static void Main(string[] args)
         {
-            double angleInDegees = 15;
-            double angleInRadians = angleInDegees * Math.PI / 180;
-            double sin = Math.Sin(angleInRadians);
-            double cos = Math.Cos(angleInRadians);
+            double angleInDegees;
 
-            Console.WriteLine("sin(15°) = " + Math.Round(sin, 3));
-            Console.WriteLine("cos(15°) = " + Math.Round(cos, 3));
-
-            angleInDegees = 37;
-            angleInRadians = angleInDegees * Math.PI / 180;
-            sin = Math.Sin(angleInRadians);
-            cos = Math.Cos(angleInRadians);
-
-            Console.WriteLine("sin(37°) = " + Math.Round(sin, 3));
-            Console.WriteLine("cos(37°) = " + Math.Round(cos, 3));
-
-            angleInDegees = 113;
-            angleInRadians = angleInDegees * Math.PI / 180;
-            sin = Math.Sin(angleInRadians);
-            cos = Math.Cos(angleInRadians);
-
-            Console.WriteLine("sin(113°) = " + Math.Round(sin, 3));
-            Console.WriteLine("cos(113°) = " + Math.Round(cos, 3));
+            PrintSinusCosinus(15);
+            PrintSinusCosinus(37);
+            PrintSinusCosinus(113);
 
             Console.WriteLine();
             Console.WriteLine("Введите значение угла в градусах");
             angleInDegees = double.Parse(Console.ReadLine());
-
-            angleInRadians = angleInDegees * Math.PI / 180;
-            sin = Math.Sin(angleInRadians);
-            cos = Math.Cos(angleInRadians);
-
-            Console.WriteLine("sin(" + angleInDegees + "°) = " + Math.Round(sin, 3));
-            Console.WriteLine("cos(" + angleInDegees + "°)) = " + Math.Round(cos, 3));
+            PrintSinusCosinus(angleInDegees);
 
             Console.ReadKey();
+        }
+
+        private static void PrintSinusCosinus(double angleInDegees)
+        {
+            double angleInRadians = angleInDegees * Math.PI / 180;
+            double sin = Math.Sin(angleInRadians);
+            double cos = Math.Cos(angleInRadians);
+
+            Console.WriteLine("sin(" + angleInDegees + "°) = " + Math.Round(sin, 3));
+            Console.WriteLine("cos(" + angleInDegees + "°) = " + Math.Round(cos, 3));
         }
     }
 }
