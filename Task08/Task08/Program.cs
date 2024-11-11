406dc6169206b62e3c7e375322cee464e01afd08
Belov-AI <Belov.Alexander@urfu.ru>
2024-11-11 12:53:40 +0500

    Метод-функция с локальной переменной

---
+++ Task08/Task08/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task08
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значение аргумента функции");
+            var x = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"f({x} = {MyFunction(x)}");
+
+            Console.ReadKey();
+        }
+
+        static double MyFunction(double x)
+        {
+            double result;
+
+            if (x < -1)
+                result = Math.Atan(x);
+            else if (x > 1)
+                result = 1 / x;
+            else
+                result = Math.Acos(x);
+            
+            return result;
+        }
+    }
+}
