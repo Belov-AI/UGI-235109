c9475cc0fb394ec6d0d12689d23055f11088b8d7
Belov-AI <Belov.Alexander@urfu.ru>
2024-12-03 12:11:31 +0500

    Добавлены задания 10-5 и 10-6

---
+++ Task10-5/Task10-5/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task10_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите натуральное число");
+
+            int number;
+
+            if(!int.TryParse(Console.ReadLine(), out number) || number < 1) 
+            {
+                Console.WriteLine("Ошибка ввода");
+                Console.ReadKey();
+                return;
+            }
+
+            Console.Write($"Делители {number}: ");
+
+            //в общем случае делители будут найдены не в порядке возрастания,
+            //зато количество итераций существенно сократится:
+            //если n = ab и a <= b, то a <= sqrt(n)
+
+            var sqrtN = Math.Sqrt(number); //вычислим заранее верхнюю границу для делителя,
+                                           //чтобы не вычислять её при каждой проверке
+                                           //условия выхода из цикла
+
+            for(var a = 1; a <= sqrtN; a++)
+            {
+                if(number % a == 0)
+                {
+                    var b = number / a;
+                    if (b != a)
+                        Console.Write($"{a}, {b}, ");
+                    else
+                        Console.Write($"{a}, ");
+                }
+            }
+
+            Console.WriteLine("\b\b "); //стираем запятую после последнего делителя
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task10-6/Task10-6/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task10_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 0;
+            if (!TryInputNumber("Введите число n", out n))
+            {
+                Console.ReadKey();
+                return;
+            }
+            
+            if(n < 1)
+            {
+                Console.WriteLine("Число n должно быть натуральным");
+                Console.ReadKey();
+                return;
+            }
+
+            int k = 0;
+            if (!TryInputNumber("Введите число k", out k))
+            {
+                Console.ReadKey();
+                return;
+            }
+
+            if (k < 1)
+            {
+                Console.WriteLine("Число k должно быть натуральным");
+                Console.ReadKey();
+                return;
+            }
+
+            int counter = 0;
+
+            for(var number = 1; number <= n; number++)
+            {
+                var temp = number;
+                var sum = 0;
+
+                while (temp > 0)
+                {
+                    sum += temp % 10;
+                    temp /= 10;
+                }
+
+                //будем печатать числа в строку через запятую
+                if (sum == k)
+                {
+                    Console.Write($"{number}, ");
+                    counter++; //считаем, сколько чисел нашли
+                }
+                    
+            }
+
+            if (counter == 0)
+                Console.WriteLine($"Натуральных чисел, не превосходящих  {n} с суммой цифр {k} нет");
+            else 
+                Console.WriteLine("\b\b "); //стираем последнюю запятую
+
+            Console.ReadKey();
+        }
+
+        static bool TryInputNumber(string message, out int number)
+        {
+            Console.WriteLine(message);
+            var input = Console.ReadLine();
+
+            if (!int.TryParse(input, out number))
+            {
+                Console.WriteLine("Ошибка ввода");
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
